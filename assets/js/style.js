a0a0da3e8c155f6aa1e09d847a1799e914271a19
Leah Russell <leahsigridrussell@gmail.com>
2021-03-14 16:20:35 -0700

    added icons

--- assets/js/style.js
+++ assets/js/style.js
@@ -59,14 +59,17 @@ var getCurrentWeather = function(lat, lon, city) {
             var humidity = data.current.humidity;
             var windSpeed = data.current.wind_speed;
             var uvIndex = data.daily[0].uvi;
-
+            var iconURL = "http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png";
 
             // display city, date and icon
             let cityNameEl = document.createElement("h2");
-            cityNameEl.textContent = city + " (" + today + ") **INSERT ICON HERE**";
+            cityNameEl.textContent = city + " (" + today + ")";
+
+            let iconEl = document.createElement("img");
+            iconEl.setAttribute("src", iconURL);
             // display temp
             let tempEl = document.createElement("p");
-            tempEl.textContent = "Temperature: " + temp + " F";
+            tempEl.innerHTML = "Temperature: " + temp + " &deg;F";
 
             // display humidity
             let humidityEl = document.createElement("p");
@@ -78,9 +81,15 @@ var getCurrentWeather = function(lat, lon, city) {
 
             // display uv index
             let uvIndexEl = document.createElement("p");
+            let uvIndexSpan = document.createElement("span");
+
             uvIndexEl.textContent = "UV Index: " + uvIndex;
+            uvIndex.textContent = uvIndex;
+
+            uvIndexIndicator(uvIndex);
 
             cityContainerEl.appendChild(cityNameEl);
+            cityNameEl.appendChild(iconEl);
             cityContainerEl.appendChild(tempEl);
             cityContainerEl.appendChild(humidityEl);
             cityContainerEl.appendChild(windSpeedEl);
@@ -89,7 +98,9 @@ var getCurrentWeather = function(lat, lon, city) {
             // 5-DAY Forecast
             for (var i = 1; i < 6; i++) {
                 // forecast day elements
-                let forecastEl = document.querySelector("#forecast-" + i);                let forecastDay = data.daily[i];
+                let forecastEl = document.querySelector("#forecast-" + i);
+                let forecastDay = data.daily[i];
+                var dailyIconURL = "http://openweathermap.org/img/w/" + forecastDay.weather[0].icon + ".png";
 
                 // current date
                 let timestamp = forecastDay.dt;
@@ -105,18 +116,21 @@ var getCurrentWeather = function(lat, lon, city) {
                 dateEl.textContent = date;
 
                 // Icon
+                let dailyIconEl = document.createElement("img");
+                dailyIconEl.setAttribute('src', dailyIconURL);
 
                 // Temp
                 let dailyTemp = forecastDay.temp.day;
-                let dailyTempEl = document.createElement("h5");
-                dailyTempEl.textContent = dailyTemp + " F";
+                let dailyTempEl = document.createElement("p");
+                dailyTempEl.innerHTML = "Temp: " + dailyTemp + " &deg;F";
 
                 // Humidity
                 let dailyHumidity = forecastDay.humidity;
-                let dailyHumidityEl = document.createElement("h5");
-                dailyHumidityEl.textContent = dailyHumidity;
+                let dailyHumidityEl = document.createElement("p");
+                dailyHumidityEl.textContent = "Humidity: " + dailyHumidity;
 
                 forecastEl.appendChild(dateEl);
+                forecastEl.appendChild(dailyIconEl);
                 forecastEl.appendChild(dailyTempEl);
                 forecastEl.appendChild(dailyHumidityEl)
             };
@@ -125,11 +139,16 @@ var getCurrentWeather = function(lat, lon, city) {
 };
 
 var saveSearchedCities = function(city) {
-    var savedCitiesBtn = document.createElement("button")
-    savedCitiesBtn.textContent= city;
+    var savedCitiesBtn = document.createElement("button");
+    savedCitiesBtn.textContent = city;
 
-    searchedCitiesContainer.appendChild(savedCitiesBtn);
-    searchedCities.push(city);
+    if (searchedCities.includes(city)) {
+        return;
+    }
+    else {
+        searchedCitiesContainer.appendChild(savedCitiesBtn);
+        searchedCities.push(city);
+    };
 
     console.log(searchedCities);
 
@@ -141,16 +160,24 @@ var saveSearchedCities = function(city) {
     localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
 };
 
-
+var uvIndexIndicator = function(uvIndex) {
+    // favorable 0-2
+    if (uvIndex >= 0 && uvIndex < 3) {
+        console.log('favorable');
+    }
+    // moderate 3-5
+    else if (uvIndex >= 3 && uvIndex < 5) {
+        console.log("moderate");
+    }
+    // severe 8-10
+    else {
+        console.log("severe");
+    }
+};
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
 
 // GIVEN a weather dashboard with form inputs
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// localStorage
